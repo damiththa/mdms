96315a149638bd69c72eb6b702cefd8e8d7f110f
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2016-02-12 16:06:28 -0500

    working on promise return

--- app/Controllers/charityController.js
+++ app/Controllers/charityController.js
@@ -1,13 +1,11 @@
 (function () {
     "use strict";
     var charityController = function ($scope, wikiService, $http) {
-//        var CharityWater_PromiseReturn = wikiService.getCharityWater();
-//        CharityWater_PromiseReturn.then(function (data){
-//            $scope.CharityWater_body = data;
-//            console.log($scope.CharityWater_body);
-//        });
-        
-        console.log('asfasfsasfas');
+        var CharityWater_PromiseReturn = wikiService.getCharityWater();
+        CharityWater_PromiseReturn.then(function (data){
+            $scope.CharityWater_data = data;
+            console.log($scope.CharityWater_data.data.query.pages[0].extract);
+        });        
     };
     
     charityController.$inject = ['$scope', 'wikiService', '$http'];
