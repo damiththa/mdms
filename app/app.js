e717ca43837a52b1b959f99b0c1f87c808815372
unknown <madushan@WistarIT-MDM.Wistar.upenn.edu>
2016-02-12 12:29:37 -0500

    referencing the newly created charity controller

--- app/app.js
+++ app/app.js
@@ -14,6 +14,7 @@
                 templateUrl: 'app/Views/career.html'
             })
             .when('/charity', {
+                controller: 'app/Controllers/charityController',
                 templateUrl: 'app/Views/charity.html'
             })
             .when('/education', {
