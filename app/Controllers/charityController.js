62ff14106a17c7147a8289f2001210a3585e5c6a
Madushan Memmendarachchi <damiththa@gmail.com>
2016-02-26 12:26:31 -0500

    added jQuery/bootstrap function 'popover'

--- app/Controllers/charityController.js
+++ app/Controllers/charityController.js
@@ -1,16 +1,22 @@
 (function () {
     "use strict";
     var charityController = function ($scope, wikiService, $http) {
+        
+        $scope.load = function(){
+            jQuery('#popoverData').popover();
+            jQuery('#popoverOption').popover({ trigger: "hover" });
+        };
+        $scope.load();
+        
         var CharityWater_PromiseReturn = wikiService.getCharityWater();
         CharityWater_PromiseReturn.then(function (data){
             $scope.CharityWater_data = data;
             console.log($scope.CharityWater_data.data.query.pages[0].extract);
-        });        
+        });
     };
     
     charityController.$inject = ['$scope', 'wikiService', '$http'];
     
     angular.module('appMDMS')
-        .controller('charityController', charityController);
-    
+        .controller('charityController', charityController);    
 }());
