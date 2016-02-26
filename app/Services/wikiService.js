2047e5eb210d966d17189230a705ec21935a1972
Madushan Memmendarachchi <damiththa@gmail.com>
2016-02-26 16:50:26 -0500

    Added Movember

--- app/Services/wikiService.js
+++ app/Services/wikiService.js
@@ -12,6 +12,17 @@
                 });
                 
                 return deferObject_CharityWater.promise;
+            },
+            getMovember: function(){
+                var deferObject_Movember;
+                var Movember_promise = $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&formatversion=2&prop=extracts&titles=Movember&exintro=true&exsentences=2&explaintext=true&format=json&callback=JSON_CALLBACK');
+                deferObject_Movember = deferObject_Movember || $q.defer();
+                
+                Movember_promise.then(function(data){
+                    deferObject_Movember.resolve(data);
+                });
+                
+                return deferObject_Movember.promise;
             }
         };
         return wikiMethods;
