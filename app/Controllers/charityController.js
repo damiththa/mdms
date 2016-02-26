1d3faaaac5a8a4789d4a5d95b260431a17157f8f
Madushan Memmendarachchi <damiththa@gmail.com>
2016-02-26 16:50:15 -0500

    added Movember

--- app/Controllers/charityController.js
+++ app/Controllers/charityController.js
@@ -2,17 +2,30 @@
     "use strict";
     var charityController = function ($scope, wikiService, $http) {
         
-        $scope.load = function(){
+        $scope.CharityWater_Intro = 'Info. from Wikipedia';
+        
+        $scope.load = function(){            
             jQuery('#popoverData').popover();
             jQuery('#popoverOption').popover({ trigger: "hover" });
+            
+            //Charity Water
+            var CharityWater_PromiseReturn = wikiService.getCharityWater();
+            CharityWater_PromiseReturn.then(function (data){
+                $scope.CharityWater_data = data;
+//                console.log($scope.CharityWater_data.data.query.pages[0].extract);           
+                $scope.CharityWater_Intro =$scope.CharityWater_data.data.query.pages[0].extract;
+            });
+            
+            //Movember
+            var Movember_PromiseReturn = wikiService.getMovember();
+            Movember_PromiseReturn.then(function (data){
+                $scope.Movember_data = data;
+//                console.log($scope.Movember_data.data.query.pages[0].extract);           
+                $scope.Movember_Intro =$scope.Movember_data.data.query.pages[0].extract;
+            });
         };
         $scope.load();
         
-        var CharityWater_PromiseReturn = wikiService.getCharityWater();
-        CharityWater_PromiseReturn.then(function (data){
-            $scope.CharityWater_data = data;
-            console.log($scope.CharityWater_data.data.query.pages[0].extract);
-        });
     };
     
     charityController.$inject = ['$scope', 'wikiService', '$http'];
