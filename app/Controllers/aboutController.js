d0fe9e0f3dfb1ce41e86290e493fc4811398385a
Madushan Memmendarachchi <damiththa@gmail.com>
2016-02-29 14:37:13 -0500

    Newly created aboutController

---
+++ app/Controllers/aboutController.js
@@ -0,0 +1,36 @@
+(function () {
+    "use strict";
+    var aboutController = function ($scope, wikiService, $http) {
+        
+        $scope.CharityWater_Intro = 'Charity water info. from Wikipedia';
+        $scope.Movember_Intro = 'Movember info. from Wikipedia';
+        
+        $scope.load = function(){            
+            jQuery('#popoverData').popover();
+            jQuery('#popoverOption').popover({ trigger: "hover" });
+            
+            //Charity Water
+            var CharityWater_PromiseReturn = wikiService.getCharityWater();
+            CharityWater_PromiseReturn.then(function (data){
+                $scope.CharityWater_data = data;
+//                console.log($scope.CharityWater_data.data.query.pages[0].extract);           
+                $scope.CharityWater_Intro =$scope.CharityWater_data.data.query.pages[0].extract;
+            });
+            
+            //Movember
+            var Movember_PromiseReturn = wikiService.getMovember();
+            Movember_PromiseReturn.then(function (data){
+                $scope.Movember_data = data;
+//                console.log($scope.Movember_data.data.query.pages[0].extract);           
+                $scope.Movember_Intro =$scope.Movember_data.data.query.pages[0].extract;
+            });
+        };
+        $scope.load();
+        
+    };
+    
+    aboutController.$inject = ['$scope', 'wikiService', '$http'];
+    
+    angular.module('appMDMS')
+        .controller('aboutController', aboutController);    
+}());
